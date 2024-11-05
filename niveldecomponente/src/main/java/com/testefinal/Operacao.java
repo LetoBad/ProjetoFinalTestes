388ffb99d5725e3efd244a03dc48a4e78697643e
user1 <valhallamaia@gmail.com>
2024-11-05 20:08:00 -0300

    commit calcualdora

---
+++ niveldecomponente/src/main/java/com/testefinal/Operacao.java
@@ -0,0 +1,20 @@
+package com.testefinal;
+
+public class Operacao {
+
+    public int somar(int x, int y) {
+        return x + y;
+    }
+
+    public int subtrair(int x, int y) {
+        return x - y;
+    }
+
+    public int multiplicar(int x, int y) {
+        return x * y;
+    }
+
+    public int dividir(int x, int y) {
+        return x / y;
+    }
+}
